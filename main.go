bb69b8e0161330d49e5c5f8fff4dccaa3cd68c49
somyagarg94 <gargsoumya71@gmail.com>
2018-02-02 14:51:45 -0500

    Add more HTTP handlers

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var addr = flag.String("addr", ":12345", "The address to listen on for HTTP requests.")
+
 func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/hello:{name}", GetHelloName).Methods("GET")
-	log.Fatal(http.ListenAndServe(":12345", router))
+	router.HandleFunc("/health", HealthChecker).Methods("GET")
+	router.HandleFunc("/counts", GetCounts).Methods("GET")
+	router.HandleFunc("/counts", DeleteCounts).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(*addr, router))
 }
