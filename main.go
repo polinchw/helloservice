a94d6d03f51346bc9c2b1d83241edeafa44c806a
somyagarg94 <gargsoumya71@gmail.com>
2018-02-02 15:16:35 -0500

    Add pkg dependency

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/somyagarg94/helloservice/pkg"
 )
 
 var addr = flag.String("addr", ":12345", "The address to listen on for HTTP requests.")
@@ -13,9 +14,9 @@ var addr = flag.String("addr", ":12345", "The address to listen on for HTTP requ
 func main() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/hello:{name}", GetHelloName).Methods("GET")
-	router.HandleFunc("/health", HealthChecker).Methods("GET")
-	router.HandleFunc("/counts", GetCounts).Methods("GET")
-	router.HandleFunc("/counts", DeleteCounts).Methods("DELETE")
+	router.HandleFunc("/hello:{name}", pkg.GetHelloName).Methods("GET")
+	router.HandleFunc("/health", pkg.HealthChecker).Methods("GET")
+	router.HandleFunc("/counts", pkg.GetCounts).Methods("GET")
+	router.HandleFunc("/counts", pkg.DeleteCounts).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(*addr, router))
 }
