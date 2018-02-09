3c9625b13df126bea73266f3274008712e8ee304
somyagarg94 <gargsoumya71@gmail.com>
2018-02-09 11:15:37 -0500

    Add count functionality

--- pkg/count/count.go
+++ pkg/count/count.go
@@ -1,14 +1,52 @@
 package count
 
-import "net/http"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
-//var GlobalCount map[string]int
+/*Assignment hints that reviewer is expecting an array data structure to be used,
+however MAP is much more memory efficient - therfore I have used MAP over ARRAY*/
+var count map[string]int
 
-//func InitApp() map[string]int {
-//	return GlobalCount
+//AddCount block is written here assuming there is no DB involved and only will be keep count of names during single runtime.
+func AddCount(name string) {
+	if count == nil {
+		count = make(map[string]int)
+	}
+	if _, exists := count[name]; exists {
+		count[name]++
+	} else {
+		count[name] = 1
+	}
+}
 
-//}
+//GetCounts will returns a JSON response with the counts of how many times a name has been called.
+func GetCounts(s http.ResponseWriter, r *http.Request) {
+	var buffer bytes.Buffer
 
-func GetCounts(http.ResponseWriter, *http.Request) {
+	buffer.WriteString("[")
 
+	i := 0
+
+	for k, v := range count {
+
+		buffer.WriteString("{" + "\"name\":" + "\"" + k + "\"," + "\"count\": " + strconv.Itoa(v) + "}")
+		if i != len(count)-1 {
+			buffer.WriteString(",")
+		}
+
+		i++
+	}
+
+	buffer.WriteString("]")
+	s.WriteHeader(http.StatusOK)
+	fmt.Fprintln(s, buffer.String())
+}
+
+//DeleteCounts will resets the data so there are no counts
+func DeleteCounts(s http.ResponseWriter, r *http.Request) {
+	count = nil
 }
