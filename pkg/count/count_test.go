acdad03948d8a9a40a1600a4212785b37275fd54
somyagarg94 <gargsoumya71@gmail.com>
2018-02-12 13:01:01 -0500

    Add tests code

--- pkg/count/count_test.go
+++ pkg/count/count_test.go
@@ -1,7 +1,24 @@
 package count
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+)
 
 func TestGetCount(t *testing.T) {
+	req, err := http.NewRequest("GET", "/counts", nil)
+	if err != nil {
+		t.Fatalf("could not created request: %v", err)
+	}
+
+	res := httptest.NewRecorder()
+	GetCounts(res, req)
+	for k, v := range count {
+		if res.Body.String() == ("{" + "\"name\":" + "\"" + k + "\"," + "\"count\": " + strconv.Itoa(v) + "}") {
+			t.Error(res.Body.String())
+		}
+	}
 
 }
--- pkg/health/health_test.go
+++ pkg/health/health_test.go
@@ -1,7 +1,24 @@
 package health
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestMapCheckerToResponse(t *testing.T) {
+func TestHealthChecker(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res := httptest.NewRecorder()
+	GetHealthChecker(res, req)
+	// Check the result code is what we expect.
+	if result := res.Code; result != http.StatusOK {
+		t.Errorf("HealthChecker returned wrong result: got %v want %v",
+			result, http.StatusOK)
+	}
 
 }
